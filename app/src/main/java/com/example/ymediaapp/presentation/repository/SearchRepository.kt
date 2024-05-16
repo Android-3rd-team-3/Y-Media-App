da96e397d1d5731dced5d45e38e978b4fd60d429
WinterWood0118 <dltjdals125@gmail.com>
2024-05-16 15:16:48 +0900

    수정사항 repository 인터페이스 수정

--- app/src/main/java/com/example/ymediaapp/presentation/repository/SearchRepository.kt
+++ app/src/main/java/com/example/ymediaapp/presentation/repository/SearchRepository.kt
@@ -8,7 +8,7 @@ interface SearchRepository {
     // 인기 동영상 받아오기
     suspend fun getPopularList(): YoutubeVideoResultEntity
     // 카테고리 비디오 목록 받아오기 - categoryEntity의 id 값 이용
-    suspend fun getVideoByCategoryList(categoryId: Int): YoutubeVideoResultEntity
+    suspend fun getVideoByCategoryList(categoryId: String): YoutubeVideoResultEntity
     // 카테고리 채널 목록 받아오기 - channel id를 "," 으로 구분지어서 스트링으로 만들어 한번에 넘기기
     suspend fun getChannelByCategoryList(channelIds: String): YoutubeChannelResultEntity
     // 카테고리 목록 받아오기(assignment true만?)
