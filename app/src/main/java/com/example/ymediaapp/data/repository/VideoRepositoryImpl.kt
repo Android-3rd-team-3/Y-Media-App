d5b2d3f410ef1e6c1367e2945a905d1a9fecc9f9
jeong-gyeongwoo <okbamtol@naver.com>
2024-05-17 21:23:10 +0900

    videoRepository pr 수정

--- app/src/main/java/com/example/ymediaapp/data/repository/VideoRepositoryImpl.kt
+++ app/src/main/java/com/example/ymediaapp/data/repository/VideoRepositoryImpl.kt
@@ -1,13 +1,21 @@
 package com.example.ymediaapp.data.repository
 
+import android.content.Context
 import com.example.ymediaapp.data.database.RoomDao
 import com.example.ymediaapp.data.database.RoomEntity
+import com.example.ymediaapp.data.database.YoutubeRoomDatabase
 import com.example.ymediaapp.presentation.entity.YoutubeVideoEntity
 import com.example.ymediaapp.presentation.repository.VideoRepository
 
-class VideoRepositoryImpl(val roomDao: RoomDao): VideoRepository {
+class VideoRepositoryImpl( val context: Context): VideoRepository {
 
+    lateinit var roomDB: YoutubeRoomDatabase
+    lateinit var roomDao: RoomDao
     override fun getVideoData(): List<YoutubeVideoEntity> {
+
+        roomDB = YoutubeRoomDatabase.getInstance(context)!!
+        roomDao = roomDB.getRoomDao()
+
         val allRoomData = roomDao.getAllData()
         val entityList = allRoomData.map {
             YoutubeVideoEntity(
