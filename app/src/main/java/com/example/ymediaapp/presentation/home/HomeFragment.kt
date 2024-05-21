53a1b0e2f005e973dd56d0e5b32f898ff2dc1db5
WinterWood0118 <dltjdals125@gmail.com>
2024-05-21 19:54:08 +0900

    import 정리

--- app/src/main/java/com/example/ymediaapp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/ymediaapp/presentation/home/HomeFragment.kt
@@ -6,17 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ymediaapp.app.AppContainer
 import com.example.ymediaapp.app.HomeContainer
-import com.example.ymediaapp.app.MyVideoContainer
 import com.example.ymediaapp.app.YMediaApplication
 import com.example.ymediaapp.databinding.FragmentHomeBinding
 import com.example.ymediaapp.presentation.model.CategoryModel
 import com.example.ymediaapp.presentation.model.YoutubeVideoModel
-import com.example.ymediaapp.presentation.my_video.MyVideoViewModel
 
 
 class HomeFragment : Fragment() {
