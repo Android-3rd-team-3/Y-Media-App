8a3e1c539056c4e98f76256175e0bf6bd4924d13
Lee HuiGwang <lhk9805@gmail.com>
2024-05-21 21:34:16 +0900

    refactor: Presentation에 정의된 model 사용하도록 로직 리팩터링

--- app/src/main/java/com/example/ymediaapp/presentation/my_video/MyVideoViewModel.kt
+++ app/src/main/java/com/example/ymediaapp/presentation/my_video/MyVideoViewModel.kt
@@ -3,18 +3,23 @@ package com.example.ymediaapp.presentation.my_video
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ymediaapp.domain.entity.YoutubeVideoEntity
 import com.example.ymediaapp.domain.repository.VideoRepository
+import com.example.ymediaapp.presentation.model.YoutubeVideoModel
+import com.example.ymediaapp.presentation.model.toModel
 import kotlinx.coroutines.launch
 
 class MyVideoViewModel(
     private val videoRepository: VideoRepository
 ): ViewModel() {
 
-    val favoriteList: LiveData<List<YoutubeVideoEntity>> = videoRepository.getVideoData()
+    val favoriteList: LiveData<List<YoutubeVideoModel>> = videoRepository.getVideoData().map { list ->
+        list.map {
+            it.toModel()
+        }
+    }
 
     fun addItem(item: YoutubeVideoEntity) = viewModelScope.launch {
         videoRepository.insertVideoData(item)
