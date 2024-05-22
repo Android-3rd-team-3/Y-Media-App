62ad04e716c2e79d47621c9b2dd95f57cc794444
WinterWood0118 <dltjdals125@gmail.com>
2024-05-22 17:46:19 +0900

    Unused Import 정리

--- app/src/main/java/com/example/ymediaapp/presentation/home/HomeChannelAdapter.kt
+++ app/src/main/java/com/example/ymediaapp/presentation/home/HomeChannelAdapter.kt
@@ -5,14 +5,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.ymediaapp.databinding.RvItemHomeBinding
-import com.example.ymediaapp.domain.entity.YoutubeChannelEntity
 import com.example.ymediaapp.presentation.model.YoutubeChannelModel
 
 //채널은 클릭 이벤트 없음
 class HomeChannelAdapter(
-    //private val onClick: (YoutubeChannelEntity) -> Unit
 ): RecyclerView.Adapter<HomeChannelAdapter.HomeChannelViewHolder>() {
-    var itemList = listOf<YoutubeChannelModel>(
+    var itemList = listOf(
         YoutubeChannelModel(
             "",
             "",
@@ -22,7 +20,6 @@ class HomeChannelAdapter(
     )
     class HomeChannelViewHolder(
         private val binding: RvItemHomeBinding,
-        //private val onClick: (YoutubeChannelEntity) -> Unit
     ): RecyclerView.ViewHolder(binding.root){
 
 
@@ -30,9 +27,6 @@ class HomeChannelAdapter(
             with(binding){
                 Glide.with(binding.root).load(youtubeChannelModel.thumbnail).into(ivTitle)
                 tvTitle.text = youtubeChannelModel.name
-/*                itemView.setOnClickListener {
-                    onClick(youtubeChannelEntity)
-                }*/
             }
         }
     }
