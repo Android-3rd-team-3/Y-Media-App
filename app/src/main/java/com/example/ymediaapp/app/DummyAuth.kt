1d4c77df776fee9d86f8f305b12e0ee1b1fda9b2
Lee HuiGwang <lhk9805@gmail.com>
2024-05-22 17:57:15 +0900

    design: UI 화면 비율 대응이라도 하게 수정

--- app/src/main/java/com/example/ymediaapp/app/DummyAuth.kt
+++ app/src/main/java/com/example/ymediaapp/app/DummyAuth.kt
@@ -6,7 +6,7 @@ import com.example.ymediaapp.R
 object DummyAuth {
     private val currentUser = User(
         channelName = "아보카도",
-        channelDescription = "양식당 경력 15년차\n올리고 싶은 레시피를 가끔 올립니다\n문의: asd@asd.com",
+        channelDescription = "양식당 경력 15년차\n올리고 싶은 레시피를 가끔 올립니다\n문의: asd@asd.com\n",
         profileImage = R.drawable.dummy_profile,
         backgroundImage = R.drawable.dummy_background
     )
