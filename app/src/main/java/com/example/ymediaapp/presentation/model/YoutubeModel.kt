973dce01dfce113cfeffce1152224ff38993b945
WinterWood0118 <dltjdals125@gmail.com>
2024-05-22 15:20:53 +0900

    Model, Mapper에서 isLike 제거

--- app/src/main/java/com/example/ymediaapp/presentation/model/YoutubeModel.kt
+++ app/src/main/java/com/example/ymediaapp/presentation/model/YoutubeModel.kt
@@ -8,7 +8,6 @@ data class YoutubeVideoModel( // 동영상
     val description: String, // 설명
     val videoId: String, // 해당 동영상의 ID
     val channelId: String, // 채널id
-    val isLike: Boolean = false // 좋아요 List 포함 여부
 )
 
 data class YoutubeVideoResultModel(
