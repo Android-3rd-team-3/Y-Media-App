ecc3c5ac32d7fa75e936cadf3f7a1ce95e0617e0
ji-circle <sp202401jw@gmail.com>
2024-05-23 11:21:15 +0900

    style: 코드 클린업

--- app/src/main/java/com/example/ymediaapp/data/repository/VideoRepositoryImpl.kt
+++ app/src/main/java/com/example/ymediaapp/data/repository/VideoRepositoryImpl.kt
@@ -1,16 +1,13 @@
 package com.example.ymediaapp.data.repository
 
 import android.content.Context
-import android.content.LocusId
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
-import androidx.lifecycle.switchMap
 import com.example.ymediaapp.data.database.RoomDao
 import com.example.ymediaapp.data.database.RoomEntity
 import com.example.ymediaapp.data.database.YoutubeRoomDatabase
 import com.example.ymediaapp.domain.entity.YoutubeVideoEntity
 import com.example.ymediaapp.domain.repository.VideoRepository
-import com.example.ymediaapp.presentation.model.YoutubeVideoModel
 
 class VideoRepositoryImpl(val context: Context) : VideoRepository {
 
